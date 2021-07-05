f38984b886db442fe96e3c06827f4f4f443d7402
digitalprojections <36181294+digitalprojections@users.noreply.github.com>
2021-07-05 18:03:20 +0900

    using fullcalendar io
    examples added

---
+++ basic_views.js
@@ -0,0 +1,100 @@
+(function() {
+ "use strict";
+ 
+  kintone.events.on(['app.record.index.show'], function(event) {
+    //-------------------------------------
+    if (!event.size) {
+      console.log('events empty');
+      
+      return;
+    }
+  // レコードの一覧取得
+  $(".calendar-table-gaia").html("<div id='calendar'></div>");
+    var records = event.records;
+
+    // 取得したレコードを配列に設定する
+    var eventList = [];
+    if (records != undefined && records.length > 0) {
+      for (var index = 0; index < records.length; index++) {
+        eventList.push(
+          {
+            title : '(' + records[index].username.value[0].name + ') ' +records[index].eventname.value,
+            start : moment(records[index].startdate.value).format("YYYY-MM-DD HH:mm:ss"),
+            end   : moment(records[index].enddate.value).format("YYYY-MM-DD HH:mm:ss"),
+            color : selectColor(records[index].category.value)
+          });
+      }
+
+      $('#calendar').fullCalendar({
+        header: {
+          left: 'prev,next today',
+          center: 'title',
+          right: 'month,basicWeek,basicDay'
+        },
+        defaultDate: new Date(),
+        displayEventTime: true,
+        displayEventEnd:true,
+        editable: true,
+        timeFormat: 'HH:mm',
+        eventLimit: true, // allow "more" link when too many events
+        events: eventList // 配列表示データとして設定
+      });
+
+    }
+
+    //-------------------------------------
+    
+    return event;
+  });
+
+
+  function selectColor(category) {
+  var selectColor = 'white';
+    switch(category) {
+      case '営業 (訪問)':
+        selectColor = 'gray';
+        break;
+      case '営業 (Web)':
+        selectColor = 'silver';
+        break;
+      case '社員面談 (訪問)':
+        selectColor = 'maroon';
+        break;
+      case '社員面談 (Web)':
+        selectColor = 'blue';
+        break;
+      case '面接':
+        selectColor = 'navy';
+        break;
+      case '説明会':
+        selectColor = 'teal';
+        break;
+      case '社内会議':
+        selectColor = 'green';
+        break;
+      case '業者打合せ':
+        selectColor = 'lime';
+        break;
+      case '来客':
+        selectColor = 'aqua';
+        break;
+      case '出張':
+        selectColor = 'yellow';
+        break;
+      case '配属':
+        selectColor = 'red';
+        break;
+      case '在宅':
+        selectColor = 'fuchsia';
+        break;
+      case '有給休暇':
+        selectColor = 'olive';
+        break;
+      case 'その他':
+        selectColor = 'purple';
+        break;
+    }
+
+    return selectColor;
+  }
+})();
--- test.js
+++ test.js
@@ -12,20 +12,26 @@ function myFunction(){
         var fontColorRed = "#ff0000";        
         //Retrieve elements for the "To Do" and "status" fields
         var elCustomer = kintone.app.getFieldElements('todo');    //field code of the To Do field
-        var elAccuracy = kintone.app.getFieldElements('status');    //field code of the Status field
-        if(elAccuracy){
-           for (var i = 0; i < elAccuracy.length; i++) {
-                //Record data is stored in the event object
-                var record = event.records[i];            
-                //If the Status is "Not started", color in the To Do field and the Status field
-                if (record['status']['value'] == "Not started") {
-                    elCustomer[i].style.color = fontColorRed;
-                    elAccuracy[i].style.color = fontColorRed;
+        //var elAccuracy = kintone.app.getFieldElements('status');    //field code of the Status field
+        records = event.records;
+        if(elCustomer){
+        //    for (var i = 0; i < records.length; i++) {
+        //         //Record data is stored in the event object
+        //         record = event.records[i];            
+        //         //If the Status is "Not started", color in the To Do field and the Status field
+        //         if (records['status']['value'] == "Not started") {
+        //             elCustomer[i].style.color = fontColorRed;
+        //             //elAccuracy[i].style.color = fontColorRed;
+        //         }
+        //     }
+            console.log(elCustomer);
+            for(i in records){
+                for (var j=0; j<records[i].length; j++){
+                        console.log(records[i][j]);
+                    }
                 }
-            }
-            console.log("All good with the elAccuracy");
         }else{
-                console.log(`problem with elAccuracy being ${elAccuracy}`);
+               console.log(`problem with elCustomer being ${elCustomer}`);
         }        
     });
     console.log(kintone);
