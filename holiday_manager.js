54a0d91f47bb6de9c661b65fe53875820c620c4d
digitalprojections <36181294+digitalprojections@users.noreply.github.com>
2021-07-13 11:31:56 +0900

    holidays
    created holidays are displayed properly

--- holiday_manager.js
+++ holiday_manager.js
@@ -9,6 +9,12 @@
     kintone.events.on('app.record.index.show', function(event) {
         records = event.records;
 
+        for (record in records){
+            for(var i=0;i<records[record].length; i++){
+                if(records[record][i].holiday_radio.value=="Yes")
+                    document.getElementById("2021-07-22-calendar-gaia").classList.add("holiday");        
+            }}
+        
 
         return event;
     });
